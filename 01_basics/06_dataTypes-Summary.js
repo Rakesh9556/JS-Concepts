4a92401c428958014c1cd6ae6da7d6746c64d807
Rakesh9556 <Rakeshguru9556@gmail.com>
2024-03-22 02:40:00 +0530

    dataTypes-Summary.js added

---
+++ 01_basics/06_dataTypes-Summary.js
@@ -0,0 +1,42 @@
+//JavaScript is dynamically typed language 
+
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Symbol
+const id1 = Symbol('123')
+const id2 = Symbol('123')
+
+// console.log(id1 === id2);
+
+//BigInt
+const  bigNumber = 12345677n
+// console.log(typeof(bigNumber));
+
+
+
+//Reference Type (Non-Primitive)
+
+// Types: Arrays, Object, Functions
+
+//Arrays
+const language = ["python", "java", "c++", "javascript"];
+
+
+//Object
+
+const employee = {
+    name: "Rakesh",
+    salary: 50000,
+}
+
+// function
+
+const myFunction = function(){
+    console.log("Hello");
+}
+
+console.log(typeof myFunction); //output => function object
+
+
