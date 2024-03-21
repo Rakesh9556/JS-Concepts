5d02bc37203a54d86dac489385539de92b36c91b
Rakesh9556 <Rakeshguru9556@gmail.com>
2024-03-21 21:12:27 +0530

    conversionOperation.js added

---
+++ 01_basics/04_conversionOperation.js
@@ -0,0 +1,10 @@
+let value = 4
+let negValue = -value
+console.log(negValue);
+
+//Tricky
+console.log("1" + 2)  //string output
+console.log(1 + "2") //string output
+console.log("1" + 2 + 2)  //string output 
+console.log(2 + 2 + "1")  //string output 
+
