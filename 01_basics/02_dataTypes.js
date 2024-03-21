2fdc29ad56f4a420ae55c85623ed654f70ab842e
Rakesh9556 <Rakeshguru9556@gmail.com>
2024-03-21 20:32:38 +0530

    dataTypes.js updated

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+// Primitive
+// number
+// bigint => for bigger number 
+// string => ""
+// boolean => true or false 
+// null => standalone value 
+// undefined => empty or not defined
+// symbol => uniqueness to identfy unique components 
+
+
+// Type of => find the type of varibale
+
+console.log(typeof null);  //type => object
+console.log(typeof undefined) // type =>undefined
+
+
+//Non Primitive
+// objects
