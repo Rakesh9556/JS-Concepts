e0ece66205072d3c00443aa660a54b74846a7c8b
Rakesh9556 <Rakeshguru9556@gmail.com>
2024-03-22 02:39:09 +0530

    comparison.js added

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true //comparisons convert null to a number, treatingit as 0, thats why this is true
+
+// console.log(undefined > 0); //false
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0);  // false
+
+
+console.log("2" == 2); 
+console.log("2" === 2);  //strictly check; check with data types
